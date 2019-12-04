cd48263dc1760498425974cbf55c0dfddd9bfec6
Janak Shrestha <old_ink@hotmail.com>
2019-12-04 16:40:36 -0500

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using GrpcServer.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Calculation.CalculationClient(channel);
+
+            var addReply = await client.AddAsync(new CalcRequest { FirstNumber = 5, SecondNumber = 7 });
+            Console.WriteLine($"Add Result: {addReply.Result}");
+
+            var multiplyReply = await client.MultiplyAsync(new CalcRequest { FirstNumber = 5, SecondNumber = 7 });
+            Console.WriteLine($"Multiplication Result: {multiplyReply.Result}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GrpcServer/Services/CalculationService.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+using GrpcServer.Protos;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CalculationService : Calculation.CalculationBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public CalculationService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CalcResponse> Add(CalcRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new CalcResponse
+            {
+                Result = request.FirstNumber + request.SecondNumber
+            });
+        }
+
+        public override Task<CalcResponse> Multiply(CalcRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new CalcResponse
+            {
+                Result = request.FirstNumber * request.SecondNumber
+            });
+        }
+
+    }
+}
